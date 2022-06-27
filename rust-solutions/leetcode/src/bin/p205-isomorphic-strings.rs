727ff411a849dee3fc4cf3c70d8ad2b3664aa359
cliffs <strohs1@gmail.com>
2022-06-27 18:00:37 -0400

    leetcode p205 isomorphic strings in rust

---
+++ rust-solutions/leetcode/src/bin/p205-isomorphic-strings.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+pub fn is_isomorphic(s: String, t: String) -> bool {
+    let mut char_map = HashMap::new();
+    let mut used_values = HashSet::new();
+
+    // iter over bytes because strings are guaranteed to be ASCII
+    for (s_char, t_char) in s.bytes().zip(t.bytes()) {
+
+        match (char_map.get(&s_char), used_values.get(&t_char)) {
+            (None, None) => {
+                char_map.insert(s_char, t_char);
+                used_values.insert(t_char);
+            },
+            (None, Some(_)) => {
+                // no two characters may map to the same char
+                return false;
+            },
+            (Some(s_value), _) => {
+                // char_map must continue to map to the same character
+                if s_value != &t_char {
+                    return false;
+                }
+            },
+        }
+    }
+
+    true
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use crate::is_isomorphic;
+
+    #[test]
+    fn example1() {
+        assert_eq!(is_isomorphic("foo".to_string(), "add".to_string()), true);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(is_isomorphic("foo".to_string(), "bar".to_string()), false);
+    }
+
+    #[test]
+    fn example3() {
+        assert_eq!(is_isomorphic("paper".to_string(), "title".to_string()), true);
+    }
+
+    #[test]
+    fn example4() {
+        assert_eq!(is_isomorphic("a".to_string(), "b".to_string()), true);
+    }
+
+    #[test]
+    fn example5() {
+        assert_eq!(is_isomorphic("a".to_string(), "a".to_string()), true);
+    }
+
+    #[test]
+    fn example6() {
+        assert_eq!(is_isomorphic("badc".to_string(), "baba".to_string()), false);
+    }
+
+    #[test]
+    fn example7() {
+        assert_eq!(is_isomorphic("egcd".to_string(), "adfd".to_string()), false);
+    }
+}
