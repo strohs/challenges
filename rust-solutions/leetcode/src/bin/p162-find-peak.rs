7d1cc6fe1c46a73df86d1d92fa08c4ccfb4d22d8
cliffs <strohs1@gmail.com>
2023-10-23 22:12:01 -0400

    leetcode 162 corrected

--- rust-solutions/leetcode/src/bin/p162-find-peak.rs
+++ rust-solutions/leetcode/src/bin/p162-find-peak.rs
@@ -2,7 +2,7 @@
 /// https://leetcode.com/problems/find-peak-element/
 ///
 /// Note some of the constraints for this problem:
-/// - `nums[i] != nums[i + 1]` for all valid `i`.
+/// - `nums[i] != nums[i + 1]` for all valid `i`. Therefore, there will be at least 1 peak element
 /// - we can return any index if `nums` has multiple peaks
 ///
 /// This solution will somewhat resemble a binary search. I will recursively find the
@@ -16,18 +16,21 @@ struct Solution;
 
 impl Solution {
     pub fn find_peak_element(nums: Vec<i32>) -> i32 {
-        let mut stack = vec![nums.len() / 2];
+        let mut stack = vec![(0_usize, nums.len())]; // holds (left, right) indices to check
         loop {
-            let midi = stack.pop().expect("will always have at least one element");
-            let elem = nums[midi];
-            let left_elem = if midi == 0 { i32::MIN } else { nums[midi - 1] };
-            let right_elem = if midi == nums.len() - 1 { i32::MIN } else { nums[midi + 1] };
-            if elem > left_elem && elem > right_elem {
-                return midi as i32;
-            } else {
-                stack.push(nums[..midi].len() / 2); // push left side mid index
-                stack.push(nums[midi..].len() / 2 + midi); // push right side mid index
+            let (li,ri) = stack.pop().expect("will always have at least one peak element");
+            if li == ri { continue } // we've gone past either the leftmost or rightmost bound of nums
+
+            let mi = ((ri - li) / 2) + li;
+            let m = nums[mi];
+            let l = if mi == 0 { i32::MIN } else { nums[mi - 1] };
+            let r = if mi == nums.len() - 1 { i32::MIN } else { nums[mi + 1] };
+
+            if m >= l && m >= r {
+                return mi as i32;
             }
+            stack.push((li, mi));
+            stack.push((mi + 1, ri));
         }
     }
 }
@@ -45,6 +48,36 @@ mod tests {
     }
 
     #[test]
+    fn test_two_elem() {
+        let nums = vec![2, 1];
+        assert_eq!(Solution::find_peak_element(nums), 0);
+    }
+
+    #[test]
+    fn test_two_elem_ascending() {
+        let nums = vec![1, 2];
+        assert_eq!(Solution::find_peak_element(nums), 1);
+    }
+
+    #[test]
+    fn test_three_elem() {
+        let nums = vec![3, 2, 1];
+        assert_eq!(Solution::find_peak_element(nums), 0);
+    }
+
+    #[test]
+    fn test_min_value() {
+        let nums = vec![-2147483648];
+        assert_eq!(Solution::find_peak_element(nums), 0);
+    }
+
+    #[test]
+    fn test_max_value() {
+        let nums = vec![2147483647];
+        assert_eq!(Solution::find_peak_element(nums), 0);
+    }
+
+    #[test]
     fn test_example1() {
         let nums = vec![1, 2, 3, 1];
         assert_eq!(Solution::find_peak_element(nums), 2);
@@ -53,6 +86,12 @@ mod tests {
     #[test]
     fn test_example2() {
         let nums = vec![1, 2, 1, 3, 5, 6, 4];
+        assert_eq!(Solution::find_peak_element(nums), 5);
+    }
+
+    #[test]
+    fn test_example3() {
+        let nums = vec![6, 5, 4, 3, 2, 3, 2];
         assert_eq!(Solution::find_peak_element(nums), 5);
     }
 }
